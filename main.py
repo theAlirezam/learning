1d5e814c505cae8d56037963d3c5e744f4079210
rabert <rabert>
2022-07-13 21:23:40 +0430

    the eror was from the file name(it coudnt find the name in the another dictionory) by the way i changed the api

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import requests
-from config import url, API_KEY
+from config import url2
 import json
 
 
-def get_rates():
-    respond = requests.get(url + API_KEY)
-    if respond.status_code == 200:
-        return json.loads(respond.text)
+def get_data():
+    irespond = requests.get(url2)
+    if irespond.status_code == 200:
+        return json.loads(irespond.text)
     return None
 
 
-def archive_to_dir(filename, rates):
+def archive_to_dir(filename, data):
     with open(f'archive/{filename}.json', 'w') as f:
-        f.write(json.dumps(rates))
+        f.write(json.dumps(data))
 
 
-res = get_rates()
-# archive_to_dir(res['timestamp'], res['rates'])
-print(get_rates())
+res = get_data()
+archive_to_dir('tst1', res['data'])
+print(get_data())
+
+#
+# urll = "https://api.apilayer.com/fixer/convert?to={to}&from={from}&amount={amount}"
+#
+# payload = {}
+# headers = {
+#     "apikey": "oX92FSLGD0HlitzTTjbeFSSxvqHKUPYI"
+# }
+#
+# response = requests.request("GET", url, headers=headers, data=payload)
+#
+# status_code = response.status_code
+# result = response.text
+# # print(result)
