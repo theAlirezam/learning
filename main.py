555db4ac579e041d392dc048ad987e04f572bce5
rabert <rabert>
2022-07-13 16:32:48 +0430

    first try but we have invalid api key check it out later but my code is ok

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import requests
 from config import url, API_KEY
-respond = requests.get(url + API_KEY)
-print(respond)
+import json
+
+
+def get_rates():
+    respond = requests.get(url + API_KEY)
+    if respond.status_code == 200:
+        return json.loads(respond.text)
+    return None
+
+
+def archive_to_dir(filename, rates):
+    with open(f'archive/{filename}.json', 'w') as f:
+        f.write(json.dumps(rates))
+
+
+res = get_rates()
+# archive_to_dir(res['timestamp'], res['rates'])
+print(get_rates())
