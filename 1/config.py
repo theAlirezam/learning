1963b59b95bb0a09c9781faea72e0e4b886d7864
thealirezam <alireza1020003@gmail.com>
2022-07-14 00:08:09 +0430

    the pip packages installed venv , pip ,...

--- 1/config.py
+++ 1/config.py
@@ -1,3 +1,3 @@
-url = 'https://data.fixer.io/api/latest?access_key='
+url = 'https://data.fixer.io/api/latest?access_key=p'
 url2 = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population'
 API_KEY = 'kO6U91O3RCpmaEN6BH75gChgqMYZ4soy'
